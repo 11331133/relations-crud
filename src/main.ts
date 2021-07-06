01b05753f83f6706a3afe6bdb8bc19384484759e
11331133 <81962996+11331133@users.noreply.github.com>
2021-07-06 18:43:14 +0900

    Helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
   await app.listen(3000);
 }
+
 bootstrap();
